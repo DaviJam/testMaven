198751f23aed85ca7a6ab7b397dd482ba0d37dc4
David <daori971@gmail.com>
2021-02-19 16:24:45 +0100

    update script

--- runWebApp.ps1
+++ runWebApp.ps1
@@ -1,7 +1,7 @@
-cd C:\TP-integration-git-maven\apache-tomcat-10.0.2\bin
-start C:\TP-integration-git-maven\apache-tomcat-10.0.2\bin\startup.bat
+cd C:\TP-integration-git-maven\lib\apache-tomcat-10.0.2\bin
+start C:\TP-integration-git-maven\lib\apache-tomcat-10.0.2\bin\startup.bat
 cd C:\TP-integration-git-maven
 mvn package 
-Copy-Item -Path C:\TP-integration-git-maven\monappli-web\target\monappli-web.war -Destination C:\TP-integration-git-maven\apache-tomcat-10.0.2\webapps -PassThru
+Copy-Item -Path C:\TP-integration-git-maven\monappli-web\target\monappli-web.war -Destination C:\TP-integration-git-maven\lib\apache-tomcat-10.0.2\webapps -PassThru
 timeout /T 5 /NOBREAK 
 start chrome http://localhost:8080/monappli-web
